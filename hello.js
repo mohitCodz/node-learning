a637605a909e7e8c0797f4ed21725ba703a53758
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-27 01:05:11 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,15 @@
-console.log("Hello World ! \nI am learning Node.js Now !!!!!!1");
+// console.log("Hello World ! \nI am learning Node.js Now !!!!!!1");
+
+const fs = require('fs');
+
+// defining two variables
+let a =20;
+let b = 20;
+
+// simple arithmatic operations
+let sum = a + b;
+let product = a * b;
+
+// prepare data to write 
+let result = `Sum: ${sum}\n Product:${product}`;
+console.log(result); // displaying the result 
