7d15b6bb3fe4b8f4394fa1df5671a2288a3e5b28
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-28 22:28:15 +0530

    Update app.js

--- Node-Server/app.js
+++ Node-Server/app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
 console.log(req);
 });
-const PORT = 3002;
-server.listen(prototype,()=>{
+const PORT = 3000;
+server.listen(PORT,()=>{
     console.log(`Server running at http://:localhost:${PORT}`);
 });
