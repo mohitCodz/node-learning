55dd30db9187441f6d75c055116af735db2acdbf
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-28 20:10:01 +0530

    Update app.js

--- Node-Server/app.js
+++ Node-Server/app.js
@@ -1,13 +1,10 @@
 // creating first node server
 
 const http = require('http');
-
-// function requestListener(req,res){
-//     console.log(req);
-// }
-//using 
 const server = http.createServer((req,res)=>{
 console.log(req);
 });
-// server is listening at port number 3000
-server.listen(3000);
+const PORT = 3000;
+server.listen(prototype,()=>{
+    console.log(`Server running at http://:localhost:${PORT}`)
+});
