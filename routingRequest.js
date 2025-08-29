9e934379f09801638be0cb2863542dd83a37e05c
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 10:21:40 +0530

    Update routingRequest.js

--- routingRequest.js
+++ routingRequest.js
@@ -1,4 +1,9 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
-console.log()
+res.setHeader('Content-Type', 'text/html');
+res.write('<html>');
+res.write('<head><title>Node.js</title></head>');
+res.write('<body><h1>Keep Coding ! Keep Building Logics</h1></body>');
+res.write('</html>');
+res.end();
 });
