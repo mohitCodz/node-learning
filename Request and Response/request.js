73dabf1c778a770fa8db8cfceddaed2338b8904f
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 10:07:12 +0530

    Create request.js

---
+++ Request and Response/request.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const server = http.createServer((req,res)=>{
+console.log(req.url,req.method,req.headers);
+});
+
+const PORT = 30003; // port number 3003
+server.listen(PORT,()=>{
+console.log(`Server is running at ${PORT}`);
+});
