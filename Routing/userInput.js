e1c6f980da0b5a0074605544ba18b4b693a88e4b
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 10:51:16 +0530

    Update userInput.js

--- Routing/userInput.js
+++ Routing/userInput.js
@@ -1,15 +1,19 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-console.log(req.url,req.method,req.headers);
-if(req.url === '/')
-    res.write("<h1>Welcome to Home Page</h1>");
-    res.write('<form action="/submitDetails" meathod ="POST');
-        res.write('<input type = "text" id ="name" name="name" placeholder=""Enter your name><br><br>');
-            res.write("<h1>Welcome to Home Page</h1>");
-                res.write("<h1>Welcome to Home Page</h1>");
+const http = require("http");
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  if (req.url === "/") res.write("<h1>Welcome to Home Page</h1>");
+  res.write('<form action="/submitDetails" meathod ="POST');
+  res.write(
+    '<input type = "text" id ="name" name="name" placeholder=""Enter your name><br><br>'
+  );
+  res.write('<lable for="gender">Gender:>/lable>');
+  res.write('<input type="radio" id="male" name="gender" value =""');
+  res.write
 });
 
 const PORT = 5000;
-server.listen = (PORT,()=>{
+server.listen =
+  (PORT,
+  () => {
     console.log(`Server is listening at PORT ${PORT}`);
-});
+  });
