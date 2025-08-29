db529134135ae83d54b46ac71f64eb477dcbad58
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 10:16:54 +0530

    Update response.js

--- Request and Response/response.js
+++ Request and Response/response.js
@@ -3,10 +3,11 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
 res.setHeader('Content-Type', 'text/html');
-res.write('<html');
+res.write('<html>');
 res.write('<head><title>Node.js</title></head>');
 res.write('<body><h1>Keep Coding ! Keep Building Logics</h1></body>');
 res.write('/html');
+res.end();
 });
 const PORT = 30004;
 server.listen(PORT,()=>{
