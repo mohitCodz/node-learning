031e23d6175baa7df4e6bb0c938529f4dc4ec159
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 14:09:32 +0530

    Update userInput.js

--- Routing/userInput.js
+++ Routing/userInput.js
@@ -8,15 +8,15 @@ const server = http.createServer((req, res) => {
     res.write("<head><title>Form</title></head>");
     res.write("<body>");
 
-    res.write('<form action="submit-details" meathod="POST">');
-    //   res.write('<form action="/submitDetails" method="POST">');
+    res.write('<form action="/submit-details" method="POST">');
+    //   res.write('<form action="/submit-Details" method="POST">');
     res.write(
       '<input type="text" id="name" name="name" placeholder="Enter your name"><br><br>'
     );
     res.write('<label for="gender">Gender:</label>');
-    res.write('<input type="radio" id="male" name="gender" value="">');
+    res.write('<input type="radio" id="male" name="gender" value="male">');
     res.write('<label for="male">Male</label>');
-    res.write('<input type="radio" id="female" name="gender" value="">');
+    res.write('<input type="radio" id="female" name="gender" value="female">');
     res.write('<label for="female">Female</label>');
     res.write('<br><button type="submit">Submit</button>');
     res.write("</form>");
@@ -24,10 +24,11 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   } 
-  else if(req.url.toLowerCase() === "/submit-details" && req.meathod === "POST"){
-fs.writeFileSync('user.txt','Developer');
-res.statusCode = 302; // status code 
-res.setHeader('Location','/'); // home page redirection
+  else if(req.url.toLowerCase() === "/submit-details" && req.method === "POST"){
+    fs.writeFileSync('user.txt','Developer');
+    res.statusCode = 302; // status code 
+    res.setHeader('Location','/'); // home page redirection
+    return res.end();
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
