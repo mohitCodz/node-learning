4f55a2920755fd3f1e3cbc3c9b59f3fb62cf5391
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 10:07:15 +0530

    Delete app.js

--- Request and Response/app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const server = http.createServer((req,res)=>{
-console.log(req.url,req.method,req.headers);
-});
-
-const PORT = 30003; // port number 3003
-server.listen(PORT,()=>{
-console.log(`Server is running at ${PORT}`);
-});
