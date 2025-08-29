46c183b3ec4571d29b38f5378d2ca112d494e6d9
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 10:30:02 +0530

    Update routingRequest.js

--- routingRequest.js
+++ routingRequest.js
@@ -3,17 +3,26 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
 console.log(req.url,req.method,req.headers);
 if(req.url === '/'){
-
+res.setHeader('Content-Type', 'text/html');
+res.write('<html>');
+res.write('<head><title>Node.js</title></head>');
+res.write('<body><h1>Welcone to HOME page !</h1></body>');
+res.write('</html>');
+res.end();
 }else if( req.url === '/products'){
-
-}else{
+res.setHeader('Content-Type', 'text/html');
+res.write('<html>');
+res.write('<head><title>Node.js</title></head>');
+res.write('<body><h1>Sorry Currently no products are available to display</h1></body>');
+res.write('</html>');
+res.end();
+}
 res.setHeader('Content-Type', 'text/html');
 res.write('<html>');
 res.write('<head><title>Node.js</title></head>');
 res.write('<body><h1>Keep Coding ! Keep Building Logics</h1></body>');
 res.write('</html>');
 res.end();
-}
 });
 
 const PORT = 30005; // port 30005
