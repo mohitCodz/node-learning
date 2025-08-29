38d87ffb834873ae0b6286529aed5af46269195e
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 10:13:54 +0530

    Update response.js

--- Request and Response/response.js
+++ Request and Response/response.js
@@ -4,6 +4,7 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
 res.setHeader('Content-Type', 'text/html');
 res.write('<html');
+res.write('<head>')
 res.write('/html');
 });
 const PORT = 30004;
