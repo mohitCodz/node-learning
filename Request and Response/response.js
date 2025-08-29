121dce72f60682683d4f98f9f9bdc8aecfff30e2
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 10:10:06 +0530

    Update response.js

--- Request and Response/response.js
+++ Request and Response/response.js
@@ -4,3 +4,7 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
 console.log(res);
 });
+const PORT = 30004;
+server.listen(PORT,()=>{
+console.log(`Server is listening at PORT ${PORT}`);
+});
