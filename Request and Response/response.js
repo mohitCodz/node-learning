165d18af6522a65b28ba0a4403d00329b7aef140
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 10:18:17 +0530

    Update response.js

--- Request and Response/response.js
+++ Request and Response/response.js
@@ -6,7 +6,7 @@ res.setHeader('Content-Type', 'text/html');
 res.write('<html>');
 res.write('<head><title>Node.js</title></head>');
 res.write('<body><h1>Keep Coding ! Keep Building Logics</h1></body>');
-res.write('/html');
+res.write('</html>');
 res.end();
 });
 const PORT = 30004;
