ee179ec6e05a5e069e8ee7d4948a095a99bc8605
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 11:15:09 +0530

    Update userInput.js

--- Routing/userInput.js
+++ Routing/userInput.js
@@ -1,19 +1,32 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  
-//   if (req.url === "/") res.write("<h1>Welcome to Home Page</h1>");
-//   res.write('<form action="/submitDetails" meathod ="POST');
-//   res.write(
-//     '<input type = "text" id ="name" name="name" placeholder=""Enter your name><br><br>');
-//   res.write('<lable for="gender">Gender:>/lable>');
-//   res.write('<input type="radio" id="male" name="gender" value =""');
-//   res.write('<lable for ="male">Male</lable>');
-//   res.write('<input type="radio" id="female" name="gender" value =""');
-//   res.write('<lable for ="female">Female</lable>');
-//   res.write('<button type="submit">Submit>/nutton>');
-//   res.write("</form>");
-//   return res.end();
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Node.js</title></head>");
+    res.write("<body><h1>Welcone to HOME page !</h1></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Node.js</title></head>");
+  res.write("<body><h1>Come back again!</h1></body>");
+  res.write("</html>");
+  res.end();
+  //   if (req.url === "/") res.write("<h1>Welcome to Home Page</h1>");
+  //   res.write('<form action="/submitDetails" meathod ="POST');
+  //   res.write(
+  //     '<input type = "text" id ="name" name="name" placeholder=""Enter your name><br><br>');
+  //   res.write('<lable for="gender">Gender:>/lable>');
+  //   res.write('<input type="radio" id="male" name="gender" value =""');
+  //   res.write('<lable for ="male">Male</lable>');
+  //   res.write('<input type="radio" id="female" name="gender" value =""');
+  //   res.write('<lable for ="female">Female</lable>');
+  //   res.write('<button type="submit">Submit>/nutton>');
+  //   res.write("</form>");
+  //   return res.end();
 });
 
 const PORT = 5000;
