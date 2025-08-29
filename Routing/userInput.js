dc658a9aba2614e4f5303534c08ee192ba7c4e88
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 11:14:13 +0530

    Update userInput.js

--- Routing/userInput.js
+++ Routing/userInput.js
@@ -4,12 +4,15 @@ const server = http.createServer((req, res) => {
   if (req.url === "/") res.write("<h1>Welcome to Home Page</h1>");
   res.write('<form action="/submitDetails" meathod ="POST');
   res.write(
-    '<input type = "text" id ="name" name="name" placeholder=""Enter your name><br><br>'
-  );
+    '<input type = "text" id ="name" name="name" placeholder=""Enter your name><br><br>');
   res.write('<lable for="gender">Gender:>/lable>');
   res.write('<input type="radio" id="male" name="gender" value =""');
   res.write('<lable for ="male">Male</lable>');
- res.write('<input type="radio" id="female" name="gender" value =""');
+  res.write('<input type="radio" id="female" name="gender" value =""');
+  res.write('<lable for ="female">Female</lable>');
+  res.write('<button type="submit">Submit>/nutton>');
+  res.write("</form>");
+  return res.end();
 });
 
 const PORT = 5000;
