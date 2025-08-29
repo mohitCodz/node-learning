db95730fbaa468bdbafb933146ab6de89d872973
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 10:00:11 +0530

    Update app.js

--- Node-Server/app.js
+++ Node-Server/app.js
@@ -2,7 +2,8 @@
 
 const http = require('http');
 const server = http.createServer((req,res)=>{
-console.log(req);process.exit(); // exits event loop
+console.log(req);
+process.exit(); // exits event loop
 });
 const PORT = 3000;
 server.listen(PORT,()=>{
