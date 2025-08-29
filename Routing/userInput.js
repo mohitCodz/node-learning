57ae716067603f88b4eebb9016e00d7c225ca85a
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 10:45:09 +0530

    Update userInput.js

--- Routing/userInput.js
+++ Routing/userInput.js
@@ -5,5 +5,5 @@ console.log(req.url,req.method,req.headers);
 
 const PORT = 5000;
 server.listen = (PORT,()=>{
-    console.log("Server is listening at PORT %")
+    console.log(`Server is listening at PORT ${PORT}`);
 });
