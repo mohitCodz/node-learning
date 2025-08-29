ffade6b29c54d43391ee7c66d49b73ae61531507
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-29 11:31:11 +0530

    Update userInput.js

--- Routing/userInput.js
+++ Routing/userInput.js
@@ -2,15 +2,16 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   if (req.url === "/") {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Form</title></head>');
-    res.write('<body><h1>Enter your details</h1>');
-    res.write('</body>');
-    res.write('<form>');
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Form</title></head>");
+    res.write("<body><h1>Enter your details</h1>");
+
+    res.write("<form>");
     //   res.write('<form action="/submitDetails" meathod ="POST');
     res.write(
-      '<input type = "text" id ="name" name="name" placeholder=""Enter your name><br><br>');
+      '<input type = "text" id ="name" name="name" placeholder=""Enter your name><br><br>'
+    );
     res.write('<lable for="gender">Gender:>/lable>');
     res.write('<input type="radio" id="male" name="gender" value =""');
     res.write('<lable for ="male">Male</lable>');
@@ -18,8 +19,8 @@ const server = http.createServer((req, res) => {
     res.write('<lable for ="female">Female</lable>');
     res.write('<button type="submit">Submit>/nutton>');
     res.write("</form>");
-    return res.end();
-    res.write('</html>');
+    res.write("</body>");
+    res.write("</html>");
     return res.end();
   }
   res.setHeader("Content-Type", "text/html");
