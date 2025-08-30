71e38e8f96994a5e3b81b7aeaa2bfddd9af59ebe
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-30 14:10:10 +0530

    Update server.js

--- Chunks/Using Modules/server.js
+++ Chunks/Using Modules/server.js
@@ -1,9 +1,7 @@
 const http = require('http'); // server
-const server = http.createServer();
 const requestHandler = require('./handler'); //importing handler file to server file
-
-
+const server = http.createServer(requestHandler);
 const PORT = 5003;
 server.listen(PORT, () => {
-  console.log(`Server is listening at PORT ${PORT}`);
+    console.log(`Server is listening at PORT ${PORT}`);
 });
