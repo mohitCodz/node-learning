77dc635149ab21b81514ab27cf3fc046873798f9
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-30 22:09:14 +0530

    Update EventLoopSequence.js

--- Event Driven/EventLoopSequence.js
+++ Event Driven/EventLoopSequence.js
@@ -1,21 +1,21 @@
-console.log('1 Start of Script');
+console.log('1 Start of Script'); // display 1st 
 
 // Microtask queue (promise)
-Promise.resolve().then(()=> console.log('2 Microtask'));
+Promise.resolve().then(()=> console.log('2 Microtask')); // display 3rd
 
 // Timer queue
-setTimeout(()=>console.log('3. Timer'),0);
+setTimeout(()=>console.log('3. Timer'),0); // display 4rd
 
 // I/O queue
 const fs = require('fs');
-fs.readFile('user-details.txt',()=>console.log('4.  I/O '));
+fs.readFile('user-details.txt',()=>console.log('4.  I/O ')); // display 6th
 
 // Check queue
-setImmediate(()=>console.log('5 Intermediate'));
-
+setImmediate(()=>console.log('5 Intermediate')); // display 5th
+ 
 // Close queue
 process.on('exit',(code)=>{
-    console.log('6 Close queue');
+    console.log('6 Close queue'); // display 7th
 });
 
-console.log('Exit of the Script');
+console.log('Exit of the Script'); // display 2nd
