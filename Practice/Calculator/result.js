40f92da4f148e39217a4e6416621145858deee1d
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-30 16:44:48 +0530

    Update result.js

--- Practice/Calculator/result.js
+++ Practice/Calculator/result.js
@@ -1,15 +1,27 @@
 const { buffer } = require("stream/consumers");
 
 const sumRequestHandler = (req,res)=>{
-console.llog("In sum Request Handler",req.url);
+console.log("In sum Request Handler",req.url);
 const body = [];
 req.on('data',chunk =>body.push(chunk));
 req.on('end', chunk =>{
 const fullBody = Buffer.concat(body).toString();
 const params = new URLSearchParams(fullBody);
 const bodyObj = Object.fromEntries(params);
-const result = bodyObj.first + bodyObj.second;
+const result = Number(bodyObj.first) + Number(bodyObj.second);
 console.log(result);
+ res.setHeader('Content-type', 'text/html');
+    res.write(`
+        <html>
+<head><title>Calculator</title></head>
+<body>
+<h1>Your result is ${result}</h1>
+<a href="/">Home Page</a>
+</body>
+</html>`
+    );
+    return res.end();
 });
+
 }
 exports.sumRequestHandler = sumRequestHandler;
