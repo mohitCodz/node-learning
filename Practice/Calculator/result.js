1d3bfdaaa95f193826d0d0d487e56bccfef11b17
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-30 16:45:23 +0530

    Update result.js

--- Practice/Calculator/result.js
+++ Practice/Calculator/result.js
@@ -1,27 +1,31 @@
-const { buffer } = require("stream/consumers");
+const sumRequestHandler = (req, res) => {
+    console.log("In sum Request Handler", req.url);
 
-const sumRequestHandler = (req,res)=>{
-console.log("In sum Request Handler",req.url);
-const body = [];
-req.on('data',chunk =>body.push(chunk));
-req.on('end', chunk =>{
-const fullBody = Buffer.concat(body).toString();
-const params = new URLSearchParams(fullBody);
-const bodyObj = Object.fromEntries(params);
-const result = Number(bodyObj.first) + Number(bodyObj.second);
-console.log(result);
- res.setHeader('Content-type', 'text/html');
-    res.write(`
-        <html>
-<head><title>Calculator</title></head>
-<body>
-<h1>Your result is ${result}</h1>
-<a href="/">Home Page</a>
-</body>
-</html>`
-    );
-    return res.end();
-});
+    const body = [];
 
-}
+    req.on('data', chunk => body.push(chunk));
+
+    req.on('end', () => {
+        const fullBody = Buffer.concat(body).toString();
+        const params = new URLSearchParams(fullBody);
+        const bodyObj = Object.fromEntries(params);
+
+        const result = Number(bodyObj.first) + Number(bodyObj.second);
+
+        console.log("Result:", result);
+
+        res.setHeader('Content-Type', 'text/html');
+        res.write(`
+            <html>
+            <head><title>Calculator</title></head>
+            <body>
+                <h1>Your result is ${result}</h1>
+                <a href="/">Home Page</a>
+            </body>
+            </html>
+        `);
+        return res.end();
+    });
+};
+
 exports.sumRequestHandler = sumRequestHandler;
