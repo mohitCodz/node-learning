c1b21c063a83c6315a7671fa51a2c131ecd40885
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-30 16:05:03 +0530

    Create app.js

---
+++ Practice/Calculator/app.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const requestHandler = require('./incomingRequest')
+const server = http.createServer(requestHandler);
+const PORT = 5005;
+server.listen(PORT,()=>{
+   console.log(`Server is running at ${PORT}`);
+});
