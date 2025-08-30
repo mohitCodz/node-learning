6050c9e4425da93cd8375de1e0aafbd370592858
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-30 16:34:04 +0530

    Update result.js

--- Practice/Calculator/result.js
+++ Practice/Calculator/result.js
@@ -1,4 +1,6 @@
 const sumRequestHandler = (req,res)=>{
 console.llog("In sum Request Handler",req.url);
+const body = [];
+req.on('data',chunk =>body.push[chunk]);
 }
 exports.sumRequestHandler = sumRequestHandler;
