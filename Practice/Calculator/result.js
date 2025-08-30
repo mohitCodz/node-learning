09c14e64f5a097b248969966ec15bba03a8cb509
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-30 16:38:50 +0530

    Update result.js

--- Practice/Calculator/result.js
+++ Practice/Calculator/result.js
@@ -3,7 +3,7 @@ const { buffer } = require("stream/consumers");
 const sumRequestHandler = (req,res)=>{
 console.llog("In sum Request Handler",req.url);
 const body = [];
-req.on('data',chunk =>body.push[chunk]);
+req.on('data',chunk =>body.push(chunk));
 req.on('end', chunk =>{
 const fullBody = Buffer.concat(body).toString();
 const params = new URLSearchParams(fullBody);
