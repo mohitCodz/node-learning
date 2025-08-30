f23dbf84c234ad7f22cef8dd8a323efd99a000f3
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-30 16:18:04 +0530

    Update incomingRequest.js

--- Practice/Calculator/incomingRequest.js
+++ Practice/Calculator/incomingRequest.js
@@ -3,11 +3,29 @@ const requestHandler = (req, res) => {
     if (req.url === '/') {
         res.setHeader('Content-type', 'text/html');
         res.write(`
-        <html>);
+        <html>
 <head><title>Calculator</title></head>
-<body><h1>Welcome to simple Calculator</h1></body>
+<body>
+<h1>Welcome to simple Calculator</h1>
+<a href="/calculator">Go to Calculator</a>
+</body>
 </html>`
         );
+        return res.end();
+    }
+    else if (req.url.toLowerCase() === "/calculator"){
+       res.write(`
+        <html>
+<head><title>Calculator</title></head>
+<body>
+<h1>Calculate anything</h1>
+<form>
+<input type ="text"  placeholder = "First Number" name = "First">
+<input type ="text"  placeholder = "Second Number" name = "Second">
+<input type ="submit"
+</form>
+</body>
+</html>`
     }
 }
 exports.requestHandler = requestHandler;
