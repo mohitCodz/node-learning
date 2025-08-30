c53e81fabae7da7fb4036e82c18a71d70366dda9
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-30 16:06:54 +0530

    Update app.js

--- Practice/Calculator/app.js
+++ Practice/Calculator/app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const requestHandler = require('./incomingRequest')
+const { requestHandler } = require('./incomingRequest')
 const server = http.createServer(requestHandler);
 const PORT = 5005;
 server.listen(PORT,()=>{
