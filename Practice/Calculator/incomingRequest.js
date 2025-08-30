062c863ada0ae304a5ff68dd4e1d5b35f65014db
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-30 16:09:32 +0530

    Update incomingRequest.js

--- Practice/Calculator/incomingRequest.js
+++ Practice/Calculator/incomingRequest.js
@@ -1,4 +1,11 @@
 const requestHandler = (req,res) =>{
 console.log(req.url,req.method);
+if(req.url === '/'){
+    res.setHeader('Content-type','text/html');
+    res.write('<html>');
+res.write('<head><title>Calculator</title></head>');
+res.write('<body><h1>Welcome to simple Calculator</h1></body>');
+res.write('</html>');
+}
 }
 exports.requestHandler = requestHandler;
