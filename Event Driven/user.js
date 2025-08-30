ff190eef4dc7812fd4bc026b3e5871d1c1437f2a
mohitCodz <mohitjaryalthakur@gmail.com>
2025-08-30 21:42:01 +0530

    Update user.js

--- Event Driven/user.js
+++ Event Driven/user.js
@@ -1,3 +1,4 @@
+const { error } = require('console');
 const fs = require('fs');
 console.log('1 -> Start the Script ');
 
@@ -5,3 +6,12 @@ console.log('1 -> Start the Script ');
 console.log('2 -> Reading file Synchronously');
 const datasync = fs.readFileSync('user-details.txt','utf8');
 console.log('3 -> Synchronous read complete');
+
+// Asynchronous (non-blocking) operation
+console.log('4 -> Reading asynchronously');
+fs.readFileSync('user-details.txt','utf8',(err,dataAsync) =>{
+if (err) throw error;
+console.log('6 -> Synchronous read complete');
+});
+
+console.log('5 -> End of Scipt');
