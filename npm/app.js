4a338cf55c9ba5ed49f5da27e9aad450046638c9
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-01 11:37:48 +0530

    Update app.js

--- npm/app.js
+++ npm/app.js
@@ -4,7 +4,7 @@ const server = http.createServer((req,res)=>{
     console.log(req);
 });
 
-const PORT = 5008;
+const PORT = 5009;
 server.listen(PORT,()=>{
 console.log(`Server is listening at ${PORT}`);
 });
