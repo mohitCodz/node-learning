2406c609036b8724874861a998fade556ca6f96b
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-01 15:20:05 +0530

    Update middleWare.js

--- Express/Middleware in Express/middleWare.js
+++ Express/Middleware in Express/middleWare.js
@@ -1,10 +1,29 @@
-// adding middleware
-app.use((req,res,next)=>{
-    console.log("First Middleware");
-    next();
+// Core Modules
+const http = require('http'); // server
+
+// External Modules
+const express = require('express');
+
+const app = express(); // executing express
+
+// Local Module (assuming it exports a router)
+const requestHandler = require('./user'); 
+app.use('/user', requestHandler); // mount it on /user
+
+// First middleware
+app.use((req, res, next) => {
+  console.log("First Middleware");
+  next();
 });
 
-app.use((req,res,next)=>{
-    console.log("Second Middleware");
+// Second middleware
+app.use((req, res, next) => {
+  console.log("Second Middleware");
+  next();
 });
 
+const server = http.createServer(app);
+const PORT = 5009;
+server.listen(PORT, () => {
+  console.log(`Server is listening at PORT ${PORT}`);
+});
