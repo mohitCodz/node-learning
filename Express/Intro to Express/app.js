ca3177ae4829d93009e6a64536bd0e8bb74b1e86
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-01 14:49:12 +0530

    Update app.js

--- Express/Intro to Express/app.js
+++ Express/Intro to Express/app.js
@@ -1,2 +1,8 @@
 const http = require('http');
 const requestHandler = require('./user.js');
+
+const server = http.createServer(requestHandler);
+const PORT = 50001;
+server.listen(PORT,()=>{
+
+});
