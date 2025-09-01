29a1143b443b2392fb64f723b932583259cb1fa9
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-01 15:16:25 +0530

    Update middleWare.js

--- Express/Middleware in Express/middleWare.js
+++ Express/Middleware in Express/middleWare.js
@@ -1,4 +1,9 @@
 // adding middleware
 app.use((req,res,next)=>{
-    console.log("First Middleware")
+    console.log("First Middleware");
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log
 })
