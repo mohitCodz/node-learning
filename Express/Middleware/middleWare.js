edb47711e2c9bedac72981a21d5ad1d44b2c0d5d
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-01 15:30:46 +0530

    Create middleWare.js

---
+++ Express/Middleware/middleWare.js
@@ -0,0 +1,30 @@
+// Core Modules
+const http = require('http'); // server
+
+// External Modules
+const express = require('express');
+
+const app = express(); // executing express
+
+// Local Module (assuming it exports a router)
+const requestHandler = require('./user'); 
+app.use('/user', requestHandler); // mount it on /user
+
+// we can add middleware by using .use
+// First middleware 
+app.use((req, res, next) => {
+  console.log("First Middleware");
+  next(); // request will go to next middleWare
+});
+
+// Second middleware
+app.use((req, res, next) => {
+  console.log("Second Middleware");
+  next();
+});
+
+const server = http.createServer(app);
+const PORT = 5009;
+server.listen(PORT, () => {
+  console.log(`Server is listening at PORT ${PORT}`);
+});
