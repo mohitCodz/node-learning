5b1eb178fe19f08cb2c676b625470fe32ced4dc0
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-01 15:05:25 +0530

    Update app.js

--- Express/app.js
+++ Express/app.js
@@ -4,6 +4,8 @@ const http = require('http'); // server
 // External Modules
 const express = require('express');
 
+const app = express(); // executing express
+
 //Local Module
 const requestHandler = require('./user'); //importing 
 const server = http.createServer(requestHandler);
