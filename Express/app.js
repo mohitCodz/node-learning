270d8cbd0bb86a02178dbf4f6cc35723f4acae50
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-01 15:06:28 +0530

    Update app.js

--- Express/app.js
+++ Express/app.js
@@ -8,7 +8,7 @@ const app = express(); // executing express
 
 //Local Module
 const requestHandler = require('./user'); //importing 
-const server = http.createServer(requestHandler);
+const server = http.createServer(app);
 const PORT = 5009;
 server.listen(PORT, () => {
     console.log(`Server is listening at PORT ${PORT}`);
