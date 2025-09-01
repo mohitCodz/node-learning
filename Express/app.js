4340f0eaed41a521870456601ca47c8c2ff91db7
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-01 15:02:08 +0530

    Update app.js

--- Express/app.js
+++ Express/app.js
@@ -1,7 +1,7 @@
 const http = require('http'); // server
 const requestHandler = require('./handler'); //importing handler file to server file
 const server = http.createServer(requestHandler);
-const PORT = 5004;
+const PORT = 5009;
 server.listen(PORT, () => {
     console.log(`Server is listening at PORT ${PORT}`);
 });
