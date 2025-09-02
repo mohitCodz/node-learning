5425c8c3a60090b5c2275bd6b04ac6e0f8b0e2a1
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-02 17:03:24 +0530

    Update middleWare.js

--- Express/Middleware/middleWare.js
+++ Express/Middleware/middleWare.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
 // Second middleware
 app.use((req, res, next) => {
-  console.log("Second Middleware");
+  console.log("Second Middleware",req.url,req.method);
   next();
 });
 
