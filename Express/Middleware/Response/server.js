e1bce3ef4be4864be962ec5bfd60d2e42b8bfed6
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-02 23:03:30 +0530

    Update server.js

--- Express/Middleware/Response/server.js
+++ Express/Middleware/Response/server.js
@@ -9,7 +9,7 @@ const app = express(); // executing express
 //Local Module
 const requestHandler = require('./user'); //importing 
 const server = http.createServer(app);
-const PORT = 5009; // port number
+const PORT = 5009; // port 
 server.listen(PORT, () => {
     console.log(`Server is listening at PORT ${PORT}`);
 });
