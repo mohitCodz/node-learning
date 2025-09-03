d65f96700e0bd600dc203155c8569d4788bbd1b6
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-03 21:02:44 +0530

    Update firrstServer.js

--- Node-Server/firrstServer.js
+++ Node-Server/firrstServer.js
@@ -3,7 +3,7 @@
 const http = require('http');
 const server = http.createServer((req,res)=>{
 console.log(req);
-process.exit(); // exits event loop
+
 });
 const PORT = 3000;
 server.listen(PORT,()=>{
