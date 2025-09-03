21be15a680006436dc4cfdcd79edd4f5ce308c29
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-03 20:58:06 +0530

    Update server.js

---
+++ Express/Middleware/Response/server.js
@@ -0,0 +1,15 @@
+// Core Modules
+const http = require('http'); // server
+
+// External Modules
+const express = require('express');
+
+const app = express(); // executing express
+
+//Local Module
+const requestHandler = require('./user'); //importing 
+const server = http.createServer(app);
+const PORT = 5009; // port 
+server.listen(PORT,()=>{
+console.log(`Servre is listening at PORT${PORT}`);
+});
