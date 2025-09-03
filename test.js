c259f082518e96330d2f3ef98108edacb4bfa7c6
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-03 23:14:51 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,11 +2,11 @@
 
 const http = require('http');
 const server = http.createServer((req,res)=>{
-console.log(req.url,req.method);
+console.log(req);
 });
 
 // port
 const PORT = 300001;
-server.listen(()=>{
+server.listen(PORT()=>{
 console.log(`Server is listenign at PORT${PORT}`);
 });
