c9d08adbd9cf28781ddda2526883ac3db298103a
mohitCodz <mohitjaryalthakur@gmail.com>
2025-09-04 21:33:10 +0530

    Update handlingRoutes.js

--- Express/Middleware/Handling Routes/handlingRoutes.js
+++ Express/Middleware/Handling Routes/handlingRoutes.js
@@ -2,5 +2,5 @@ const app = express();
 app.use('/',(req,res,next)=>{
 console.log("Hello World");
 res.send('<p>Welcome to submit details page</p>');
-next();
+next(); // request will go the next middleware
 });
